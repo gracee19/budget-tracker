77f50df3f011c555ac7e59c9046ab2b97a195cc2
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-05-19 09:23:48 +0930

    style css and service-worker.js added

---
+++ public/assets/service-worker.js
@@ -0,0 +1,81 @@
+console.log("Hi from your service-worker.js file!")
+const FILES_TO_CACHE = [
+     "/",
+     "/index.html",
+   "/styles.css",
+    "/manifest.webmanifest",
+    "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/materia/bootstrap.css",
+    "https://use.fontawesome.com/releases/v5.8.2/css/all.css",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png"
+];
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+
+// Clean Cache
+self.addEventListener('activate', (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+      caches
+        .keys()
+        .then((cacheNames) => {
+          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+        })
+        .then((cachesToDelete) => {
+          return Promise.all(
+            cachesToDelete.map((cacheToDelete) => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+self.addEventListener("fetch", event => {
+    // non GET requests are not cached and requests to other origins are not cached
+    if (
+        event.request.method !== "GET" ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+    // GET requests for data from /api routes
+    if (event.request.url.includes("/api/images")) {
+        event.respondWith(
+            caches.open(RUNTIME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                    .catch(() => caches.match(event.request));
+            })
+        );
+        return;
+    }
+    // use cache first for all other requests for performance
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            // request is not in cache. make network request and cache the response
+            return caches.open(RUNTIME).then(cache => {
+                return fetch(event.request).then(response => {
+                    return cache.put(event.request, response.clone()).then(() => {
+                        return response;
+                    });
+                });
+            });
+        })
+    );
+});
